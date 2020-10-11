b6a6da321275134f10b2e327a11e9e01ffa1ca6b
ChronisG <thegamedevelopergame@gmail.com>
2020-10-11 06:41:13 +0100

    lambdas

--- 12-lambdas.hs
+++ 12-lambdas.hs
@@ -1,8 +1,11 @@
+-- lambda (\parms -> logic)
+lessThanFive x = filter (\x -> x < 5) x
+
 -- this func defines its own helper func (quotify) that is only used once
 suspiciousquotes s = unwords $ map quotify (words s)
   where quotify s = "'" ++ s ++ "'"
 
--- instead of writing a helper func you can use a lambda func (\parms -> logic) 
+-- instead of writing a helper func you can use a lambda func  
 suspiciousquotes' s = unwords $ map (\s -> "'" ++ s ++ "'") (words s)
 
 -- the quotify func can be written in many ways
